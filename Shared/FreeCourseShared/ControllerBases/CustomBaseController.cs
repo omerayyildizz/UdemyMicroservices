d088d079caa624316de28cffc535dc54cc5225be
Ömer Ayyıldız <omerayyildiz403@outlook.com>
2022-09-23 16:30:45 +0300

    Add project files.

---
+++ Shared/FreeCourseShared/ControllerBases/CustomBaseController.cs
@@ -0,0 +1,21 @@
+﻿using FreeCourseShared.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourseShared.ControllerBases
+{
+    public class CustomBaseController : ControllerBase
+    {
+        public IActionResult CreateActionResultInstance<T>(Response<T> response)
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
---
+++ Shared/FreeCourseShared/Dtos/NoContent.cs
@@ -0,0 +1,6 @@
+﻿namespace FreeCourseShared.Dtos
+{
+    public class NoContent
+    {
+    }
+}
